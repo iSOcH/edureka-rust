ebc2095cf4aeb9fb05826a2872c9d70fd1b8a0f8
iSO <iso@thinktux.net>
2025-03-02 22:14:33 +0000

    middleware

--- web-development-with-rust/mod1/rocket-first/src/main.rs
+++ web-development-with-rust/mod1/rocket-first/src/main.rs
@@ -1,16 +1,25 @@
-use rocket::{get, http::CookieJar, launch, post, put, routes};
+use mycounterfairing::MyCounterFairing;
+use rocket::http::CookieJar;
 
-const COOKIE_NAME: &str = "mycookie";
+#[macro_use] extern crate rocket;
+
+static COOKIE_NAME: &str = "mycookie";
+
+mod mycounterfairing;
 
 #[launch]
 fn rocket() -> _ {
-    let app = rocket::build().mount("/", routes![home, set_cookie, user_get, submit, user_update_name]);
+    // note that middlewares seemingly need a Fairing impl, the course does not mention this and the given code does not compile
+    // instead I used https://www.shuttle.dev/blog/2022/08/04/middleware-in-rust#rocket as a starting point
+    let app = rocket::build()
+        .attach(MyCounterFairing::new())
+        .mount("/", routes![home, set_cookie, user_get, submit, user_update_name]);
     app
 }
 
 // I fear the video is outdated. it uses a `Cookies` and `Request` type here, but both seem to not be compatible (no impl for FromRequest)
 #[get("/")]
-fn home(cookies: &CookieJar<'_>) -> String {
+fn home(cookies: &CookieJar) -> String {
     let cookie = cookies.get(COOKIE_NAME).map(|c| c.value());
     format!("Hello, your cookie contains {cookie:?}")
 }
---
+++ web-development-with-rust/mod1/rocket-first/src/mycounterfairing.rs
@@ -0,0 +1,41 @@
+use std::sync::atomic::AtomicUsize;
+
+use rocket::{fairing::{Fairing, Info, Kind}, http::{Header, Method}, Data, Request, Response};
+
+// https://www.shuttle.dev/blog/2022/08/04/middleware-in-rust#rocket
+pub struct MyCounterFairing {
+    get_requests: AtomicUsize,
+}
+
+impl MyCounterFairing {
+    pub fn new() -> MyCounterFairing {
+        MyCounterFairing {
+            get_requests: AtomicUsize::new(0)
+        }
+    }
+}
+
+#[rocket::async_trait]
+impl Fairing for MyCounterFairing {
+    fn info(&self) -> Info {
+        Info {
+            name: "GET Counter",
+            kind: Kind::Request | Kind::Response
+        }
+    }
+
+    async fn on_request(&self, request: &mut Request<'_>, _: &mut Data<'_>) {
+        if let Method::Get = request.method() {
+            self.get_requests.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, result: &mut Response<'r>) {
+        let header = Header {
+            name: "req-count".into(),
+            value: self.get_requests.load(std::sync::atomic::Ordering::Relaxed).to_string().into()
+        };
+
+        result.set_header(header);
+    }
+}
