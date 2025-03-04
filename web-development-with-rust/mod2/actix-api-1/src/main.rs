5b7be6bb4057a9e5e97af30ddac45bcb5a54f624
iSO <iso@thinktux.net>
2025-03-04 08:03:05 +0000

    "Steps to Build RESTful APIs"

---
+++ web-development-with-rust/mod2/actix-api-1/src/main.rs
@@ -0,0 +1,21 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+            .route("/echo", web::post().to(echo))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn hello() -> impl Responder {
+    "Hello, world"
+}
+
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
